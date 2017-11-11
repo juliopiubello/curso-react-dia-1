aa386b034b897ac718ea2c6be4b763eefac9f9ce
Júlio Piubello da Silva Cabral <juliopiubellow@gmail.com>
2017-11-11 19:01:41 -0200

    now the new posts are inputted by a form using material u-i and can be showed or not

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import Post from './Post'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import FlatButton from 'material-ui/FlatButton';
+import {Card, CardActions, CardText, CardHeader} from 'material-ui/Card';
+import TextField from 'material-ui/TextField';
 
 class App extends Component {
 
@@ -13,6 +15,7 @@ class App extends Component {
       key: 1,
       novoName:'',
       novoTexto:'',
+      expanded: false
     };
   }
 
@@ -39,25 +42,75 @@ class App extends Component {
     this.saveInStorage();
   }
 
+  handleSubmit = () =>{
+    let post = {
+      name: this.state.novoName,
+      text: this.state.novoTexto
+    }
+
+    let novo = this.state.posts;
+    novo.push(post);
+    this.setState({posts: novo});
+    this.saveInStorage();
+    this.zerarNomeTexto();
+  }
+
+  zerarNomeTexto= () =>{
+    this.setState({novoName: ''});
+    this.setState({novoTexto: ''});
+  }
+  
   saveInStorage(){
     let actualState = this.state;
     actualState = JSON.stringify(actualState);
     localStorage.setItem('appState', actualState);
   }
 
-  handleChange(event) {
-    this.setState({value: event.target.value});
+  handleChangeName = (event,value) => {
+    this.setState({
+      novoName: value,
+    });
+  }
+  handleChangeTexto = (event,value) => {
+    this.setState({
+      novoTexto: value,
+    });
   }
 
+  handleExpand = () => {
+    this.setState({expanded: true});
+  }
 
+  handleReduce = () => {
+    this.setState({expanded: false});
+  }
+
+  handleExpandChange = (expanded) => {
+    this.setState({expanded: expanded});
+  };
 
   render() {
-    console.log(this.state)
     return (
         <MuiThemeProvider> 
-          <div style={{padding:30, background:'#DDDDDD'}}>
+          <div style={{padding:30, background:'#CCCCCC'}}>
           <FlatButton label={'Clear all posts from storage'} onClick={this.limparStorage} />
-          <FlatButton label={'Novo post'} onClick={this.novoPost} />
+          <FlatButton label={'Novo post'} onClick={this.handleExpand} />
+          <Card expanded={this.state.expanded} onExpandChange={this.handleExpandChange} >
+          <CardText expandable={true}>
+            <div style={{padding:30,background:'#FFFFFF'}}>
+              <label> INSIRA SEU POST </label>
+              <FlatButton label={'Fechar'} onClick={this.handleReduce} />
+              <br />
+              <label> Nome </label>
+              <TextField id="text-field-nome" value={this.state.novoName} onChange={this.handleChangeName} />
+              <br />
+              <label> Texto </label>
+              <TextField id="text-field-text" value={this.state.novoTexto} onChange={this.handleChangeTexto} />
+              <br />
+              <FlatButton label="Submit" onClick={this.handleSubmit} />
+            </div>
+          </CardText>
+          </Card>
           {
             this.state.posts.map((post, index) => 
               <div key={index} >
